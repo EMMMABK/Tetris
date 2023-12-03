ea7a59b973061cd40598b5d7c66e0fa48b0dd173
EMMMABK <adilbikiev2@gmail.com>
2023-12-03 18:26:39 +0600

    add block under block

--- tetris.py
+++ tetris.py
@@ -6,10 +6,20 @@ class Tetris():
     def __init__(self, app):
         self.app = app
         self.sprite_group = pg.sprite.Group()
+        self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
     
+    def put_tetromino_blocks_in_array(self):
+        for block in self.tetromino.blocks:
+            x, y = int(block.pos.x), int(block.pos.y)
+            self.field_array[y][x] = block
+
+    def get_field_array(self):
+        return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+
     def check_tetromino_landing(self):
         if self.tetromino.landing:
+            self.put_tetromino_blocks_in_array()
             self.tetromino = Tetromino(self)
 
     def control(self, pressed_key):
--- tetromino.py
+++ tetromino.py
@@ -20,7 +20,7 @@ class Block(pg.sprite.Sprite):
 
     def is_collide(self, pos):
         x, y  = int(pos.x), int(pos.y)
-        if 0 <= x < FIELD_W and y < FIELD_H:
+        if 0 <= x < FIELD_W and y < FIELD_H and (y < 0 or not self.tetromino.tetris.field_array[y][x]):
             return False
         return True
 
