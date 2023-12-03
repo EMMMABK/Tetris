b59aaa9f9d745f8b12b5ee9b491339c710c52784
EMMMABK <adilbikiev2@gmail.com>
2023-12-03 19:32:52 +0600

    add Sett

--- settings.py
+++ settings.py
@@ -19,6 +19,7 @@ FIELD_SCALE_W, FIELD_SCALE_H = 1.7, 1.0
 WIN_RES = WIN_W, WIN_H = FIELD_RES[0] * FIELD_SCALE_W, FIELD_RES[1] * FIELD_SCALE_H 
 
 INIT_POS_OFFSET = vec(FIELD_W // 2 - 1, 0)
+NEXT_POS_OFFSET = vec(FIELD_W * 1.3, FIELD_H * 0.45)
 MOVE_DIRECTIONS = {'left': vec(-1,0), 'right': vec(1,0), 'down': vec(0,1)} 
 
 TETROMINOES = {
