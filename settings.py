5feb20421592cd0337d5f1d3ae689bd3f9abb1b0
EMMMABK <adilbikiev2@gmail.com>
2023-12-03 17:03:49 +0600

    add Sett1

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 import pygame as pg
+
+vec = pg.math.Vector2
+
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
 
@@ -6,6 +9,8 @@ TILE_SIZE = 33
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
 
+INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
+ 
 TETROMINOES = {
     'T': [(0,0), (-1,0), (1,0), (0,-1)],
     'O': [(0,0), (0,-1), (1,0), (1,-1)],
